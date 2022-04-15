23bf14231ff7e7ee0886f66399220315a7a13c4c
ging-dev <thanh1101dev@gmail.com>
2022-04-15 20:35:00 +0700

    eager loading

--- app/Http/Livewire/DeclarationList.php
+++ app/Http/Livewire/DeclarationList.php
@@ -19,7 +19,7 @@ class DeclarationList extends Component
     public function render()
     {
         return view('livewire.declaration-list', [
-            'statuses' => VaccinationStatus::orderBy('updated_at', 'desc')
+            'statuses' => VaccinationStatus::with('user')->orderBy('updated_at', 'desc')
                 ->paginate(5),
         ]);
     }
